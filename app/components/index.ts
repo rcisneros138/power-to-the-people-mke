b3bd4f28af59f7b5fa3392c784de5df32ba10532
Raymond Cisneros <rcisneros138@gmail.com>
2026-01-16 11:11:50 -0600

    feat: add component barrel exports

---
+++ app/components/index.ts
@@ -0,0 +1,7 @@
+export { default as Header } from "./Header";
+export { default as Footer } from "./Footer";
+export { default as Hero } from "./Hero";
+export { default as BenefitsGrid } from "./BenefitsGrid";
+export { default as ContentSection } from "./ContentSection";
+export { default as CTABanner } from "./CTABanner";
+export { default as PartnersStrip } from "./PartnersStrip";
